bf8f628cab977a668f84ea73d22149e5c797a5b2
andrewelamb <andrewelamb@gmail.com>
2020-03-19 08:51:31 -0700

    various

---
+++ R/create_iatlas_fpkm_file.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(synapser)
+library(magrittr)
+
+hugo_translation_id <- "syn11536071"
+fpkm_synapse_id     <- "syn18134933"
+iatlas_sample_id    <- "syn21785582"
+output_file_name    <- "iatlas_fpkm.tsv"
+
+synapser::synLogin()
+
+hugo_translation_tbl <-  hugo_translation_id %>% 
+    synapser::synGet() %>% 
+    purrr::pluck("path") %>% 
+    data.table::fread() %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::select(feature = .data$ensembl_gene_id, gene = .data$hgnc_symbol) %>% 
+    tidyr::drop_na() %>% 
+    dplyr::filter(.data$feature != "", .data$gene != "")
+
+sample_translation_list <- iatlas_sample_id %>% 
+    synapser::synGet() %>% 
+    purrr::pluck("path") %>% 
+    data.table::fread() %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::select(.data$aliquot_id, .data$icgc_donor_id) %>% 
+    deframe %>% 
+    c(., "feature" = "feature")
+    
+fpkm_synapse_id %>% 
+    synapser::synGet() %>% 
+    purrr::pluck("path") %>% 
+    data.table::fread() %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::select(dplyr::one_of(c("feature", names(sample_translation_list)))) %>% 
+    magrittr::set_colnames(
+        ., 
+        purrr::map_chr(colnames(.,), ~sample_translation_list[[.x]])
+    ) %>% 
+    dplyr::mutate(
+        feature = stringr::str_remove_all(.data$feature, "\\.[0-9]+$")
+    ) %>% 
+    dplyr::inner_join(hugo_translation_tbl, by = "feature") %>% 
+    dplyr::select(-.data$feature) %>% 
+    dplyr::group_by(.data$gene) %>% 
+    dplyr::summarise_all(sum) %>% 
+    dplyr::ungroup() %>% 
+    readr::write_tsv(., output_file_name)
+
+activity_obj <- synapser::Activity(
+    name = "Select only samples going into iatlas, and translate to hugo genes",
+    used = list(hugo_translation_id, fpkm_synapse_id, iatlas_sample_id),
+    executed = paste0(
+        "https://github.com/CRI-iAtlas/ICGC/blob/",
+        "7d4532d0510d36dc2720120c8a96d215c8dd510e/",
+        "R/create_iatlas_fpkm_file.R"
+    )
+)
+
+output_file_name %>% 
+    synapser::File(parent = "syn18268611") %>% 
+    synapser::synStore(activity = activity_obj)
+
+file.remove(output_file_name)    
--- R/create_iatlas_sample_file.R
+++ R/create_iatlas_sample_file.R
@@ -4,30 +4,35 @@ library(magrittr)
 
 pcawg_synapse_id    <- "syn18234582"
 tcga_sample_id      <- "syn18234560"
+output_file_name    <- "iatlas_sample_sheet.tsv"
 
 synapser::synLogin()
 
 tcga_sample_ids <- tcga_sample_id %>% 
     synapser::synGet() %>% 
     purrr::pluck("path") %>% 
-    readr::read_tsv() %>% 
-    dplyr::pull(icgc_sample_id)
+    readr::read_tsv(.) %>% 
+    dplyr::pull(.data$icgc_sample_id)
 
 pcawg_tbl <- pcawg_synapse_id %>% 
     synapser::synGet() %>% 
     purrr::pluck("path") %>% 
-    readr::read_tsv() %>% 
-    filter(donor_wgs_exclusion_white_gray == "Whitelist") %>% 
-    filter(library_strategy == "RNA-Seq") %>% 
-    filter(!aliquot_id %in% tcga_sample_ids) %>% 
-    dplyr::filter(!stringr::str_detect(dcc_specimen_type, "Normal")) %>%
-    dplyr::filter(!stringr::str_detect(dcc_specimen_type, "Metastatic")) %>%
+    readr::read_tsv(.) %>% 
+    filter(.data$donor_wgs_exclusion_white_gray == "Whitelist") %>% 
+    filter(.data$library_strategy == "RNA-Seq") %>% 
+    filter(!.data$aliquot_id %in% tcga_sample_ids) %>% 
+    dplyr::filter(
+        !stringr::str_detect(.data$dcc_specimen_type, "Normal")
+    ) %>%
+    dplyr::filter(
+        !stringr::str_detect(.data$dcc_specimen_type, "Metastatic")
+    ) %>%
     dplyr::mutate(dcc_specimen_type = factor(
-        dcc_specimen_type,
+        .data$dcc_specimen_type,
         levels = c(
             "Primary tumour - solid tissue",
             "Primary tumour",
-            "Primary tumour - blood derived (bone marrow)",
+            "Primary tumour - blo od derived (bone marrow)",
             "Primary tumour - blood derived (peripheral blood)",
             "Primary tumour - lymph node",
             "Primary tumour - other",
@@ -35,15 +40,26 @@ pcawg_tbl <- pcawg_synapse_id %>%
             "Recurrent tumour - other"
         )
     )) %>%
-    dplyr::group_by(icgc_donor_id) %>%
-    dplyr::arrange(dcc_specimen_type) %>%
+    dplyr::group_by(.data$icgc_donor_id) %>%
+    dplyr::arrange(.data$dcc_specimen_type) %>%
     dplyr::slice(1) %>%
     dplyr::ungroup() %>% 
-    readr::write_tsv("iatlas_sample_sheet.csv")
+    readr::write_tsv(., output_file_name)
 
-"iatlas_sample_sheet.csv" %>% 
+activity_obj <- synapser::Activity(
+    name = "select only sampels going into iatlas",
+    used = list(pcawg_synapse_id, tcga_sample_id),
+    executed = paste0(
+        "https://github.com/CRI-iAtlas/ICGC/blob/",
+        "7d4532d0510d36dc2720120c8a96d215c8dd510e/",
+        "R/create_iatlas_sample_file.R"
+    )
+)
+
+output_file_name %>% 
     synapser::File(parent = "syn18233789") %>% 
-    synapser::synStore()
+    synapser::synStore(activity = activity_obj)
 
+file.remove(output_file_name)
 
 
